17ae944ba6761d210a4b014c49c6c3539a2a2c47
Ihor Horbonos <igorgorb@gmail.com>
2022-05-06 18:31:44 +0300

    Add template to show joke. Close #5

--- src/main/java/guru/springfrmework/spring5jokesappv2/controllers/JokeController.java
+++ src/main/java/guru/springfrmework/spring5jokesappv2/controllers/JokeController.java
@@ -1,7 +1,6 @@
 package guru.springfrmework.spring5jokesappv2.controllers;
 
 import guru.springfrmework.spring5jokesappv2.services.JokeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,6 @@ public class JokeController {
 
     private final JokeService jokeService;
 
-    @Autowired
     public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
